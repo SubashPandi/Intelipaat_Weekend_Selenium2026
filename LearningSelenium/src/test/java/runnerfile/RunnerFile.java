d34afacb77984c485e9ddc5a3902660756fe72e9
SubashPandi <subashpsna@gmail.com>
2026-01-06 15:04:41 +0530

    POM demo

--- LearningSelenium/src/test/java/runnerfile/RunnerFile.java
+++ LearningSelenium/src/test/java/runnerfile/RunnerFile.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/featurefile/SalesforeLogin.feature"},
-        glue = {"stepdefinitionfile"}
-        //tags = "@SelectIDE"
+        glue = {"stepdefinitionfile"},
+        tags = "@Smoke"
 )
 public class RunnerFile {
 
