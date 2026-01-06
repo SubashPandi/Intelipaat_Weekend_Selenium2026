9855d27118db54c80e813932ce1380bdcff45c7f
SubashPandi <subashpsna@gmail.com>
2026-01-06 18:56:34 +0530

    6janEOD_Commit

--- LearningSelenium/src/test/java/runnerfile/RunnerFile.java
+++ LearningSelenium/src/test/java/runnerfile/RunnerFile.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/featurefile/SalesforeLogin.feature"},
-        glue = {"stepdefinitionfile"},
-        tags = "@Smoke"
+        features = {"src/test/resources/featurefile/TableHandle.feature"},
+        glue = {"stepdefinitionfile"}
+        //tags = "@Smoke"
 )
 public class RunnerFile {
 
